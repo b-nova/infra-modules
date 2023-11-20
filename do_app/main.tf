87aa9a82a325e9844119d788a19607c7e8bad80d
ttrapp <tom.trapp@b-nova.com>
2023-11-20 16:58:07 +0100

    refactorings

--- do_app/main.tf
+++ do_app/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    digitalocean = {
-      source  = "digitalocean/digitalocean"
-      version = "~> 2.0"
-    }
-  }
-}
-
 resource "digitalocean_app" "digitalocean_app" {
   spec {
     name   = var.name
